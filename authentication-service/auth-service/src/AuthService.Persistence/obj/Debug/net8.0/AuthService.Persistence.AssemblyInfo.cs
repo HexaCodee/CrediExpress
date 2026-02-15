6d678b6df835c06bec7f50a00a5e49e0fa98a5f5
jsolares-2024238 <jsolares-2024238@kinal.edu.gt>
2026-02-15 13:41:10 -0600

    feat(auth-service): implement full authentication with persistence

--- authentication-service/auth-service/src/AuthService.Persistence/obj/Debug/net8.0/AuthService.Persistence.AssemblyInfo.cs
+++ authentication-service/auth-service/src/AuthService.Persistence/obj/Debug/net8.0/AuthService.Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+328c2d00727462466c7a5ece933198f32f4f4e10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb54f2247f8427ee991ec7cedd91cc80170fd024")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
